79970aee7808a0d873a3aa4fa44b6335cefd67a0
IIRenatoII <guibelr0045@gmail.com>
2026-04-22 00:29:38 -0500

    Add files via upload

---
+++ AMOLED.lua
@@ -0,0 +1,30 @@
+return {
+	Name = "AMOLED",
+	Accent = Color3.fromRGB(255, 255, 255),
+
+	AcrylicMain = Color3.fromRGB(0, 0, 0),
+	AcrylicBorder = Color3.fromRGB(20, 20, 20),
+	AcrylicGradient = ColorSequence.new(Color3.fromRGB(0, 0, 0), Color3.fromRGB(0, 0, 0)),
+	AcrylicNoise = 1,
+
+	TitleBarLine = Color3.fromRGB(28, 25, 25),
+	Tab = Color3.fromRGB(40, 40, 40),
+
+	Element = Color3.fromRGB(15, 15, 15),
+	ElementBorder = Color3.fromRGB(0, 0, 0),
+	InElementBorder = Color3.fromRGB(40, 40, 40),
+	ElementTransparency = 0.95,
+
+	DropdownFrame = Color3.fromRGB(20, 20, 20),
+	DropdownHolder = Color3.fromRGB(0, 0, 0),
+	DropdownBorder = Color3.fromRGB(0, 0, 0),
+
+	Dialog = Color3.fromRGB(0, 0, 0),
+	DialogHolder = Color3.fromRGB(00, 0, 0),
+	DialogHolderLine = Color3.fromRGB(20, 20, 20),
+	DialogButton = Color3.fromRGB(15, 15, 15),
+	DialogButtonBorder = Color3.fromRGB(30, 30, 30),
+	DialogBorder = Color3.fromRGB(27, 27, 27),
+	DialogInput = Color3.fromRGB(15, 15, 15),
+	DialogInputLine = Color3.fromRGB(60, 60, 60),
+}
